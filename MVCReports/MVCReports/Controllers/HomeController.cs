e24d0562d8e8342658e73917a75fb8035b3618a1
Rasa Cosmin <rasa.cosmin@gmail.com>
2015-08-21 12:34:02 +0300

    initial commit

---
+++ MVCReports/MVCReports/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Reporting.WebForms;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Security.Principal;
+using MVCReports.Helpers;
+
+namespace MVCReports.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+
+            ServerReport report = new ServerReport();
+                  
+
+            var reportViewer = new ReportViewer();
+
+            reportViewer.ServerReport.ReportServerCredentials = new CustomCredentials("andrei", "P@ssw0rd", "http://172.17.2.2");
+
+            reportViewer.ProcessingMode = ProcessingMode.Remote;
+
+            reportViewer.ServerReport.ReportPath = "/TableWithParameters";
+            reportViewer.ServerReport.ReportServerUrl = new Uri("http://172.17.2.2/reportserver");
+
+            
+                                 
+            ViewBag.reportView = reportViewer;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCReports/MVCReports/Helpers/CustomServerCredentials.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Web;
+
+namespace MVCReports.Helpers
+{
+    public class CustomCredentials : IReportServerCredentials
+    {
+        private string _username;
+        private string _password;
+        private string _domainName;
+
+        public CustomCredentials(string username, string password, string domain)
+        {
+            _username = username;
+            _password = password;
+            _domainName = domain;
+        }
+
+        public WindowsIdentity ImpersonationUser
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public ICredentials NetworkCredentials
+        {
+            get
+            {
+                return new NetworkCredential(_username, _password, _domainName);
+            }
+        }
+
+        public bool GetFormsCredentials(out Cookie authCookie, out string userName, out string password, out string authority)
+        {
+            authCookie = null;
+            userName = password = authority = null;
+            return false;
+        }
+    }
+}
