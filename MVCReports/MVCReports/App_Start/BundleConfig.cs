b80bd6f92b9b24b641ba649c52ed1e950875902b
zdarkmind94 <ilinca.ciprian@gmail.com>
2015-09-01 20:19:17 +0300

    style

--- MVCReports/MVCReports/App_Start/BundleConfig.cs
+++ MVCReports/MVCReports/App_Start/BundleConfig.cs
@@ -21,11 +21,17 @@ namespace MVCReports
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/app.js",
+                      "~/Scripts/respond.js"
+                     ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                       "~/Content/common.css",
+                       "~/Content/site.css",
+                       "~/Content/button.css",
+                      "~/Content/login.css"
+                      ));
         }
     }
 }
---
+++ MVCReports/MVCReports/Scripts/app.js
@@ -0,0 +1,34 @@
+﻿// Function for sticky footer
+
+$(window).bind("load", function () {
+
+    var footerHeight = 0,
+        footerTop = 0,
+        $footer = $("footer");
+
+    positionFooter();
+
+    function positionFooter() {
+
+        footerHeight = $footer.height();
+        footerTop = ($(window).scrollTop() + $(window).height() - footerHeight) + "px";
+
+        if (($(document.body).height() + footerHeight) < $(window).height()) {
+            $footer.css({
+                position: "absolute"
+            }).animate({
+                top: footerTop
+            }, 100,'linear' )
+        } else {
+            $footer.css({
+                position: "static"
+            })
+        }
+
+    }
+
+    $(window)
+            .scroll(positionFooter)
+            .resize(positionFooter)
+
+});
