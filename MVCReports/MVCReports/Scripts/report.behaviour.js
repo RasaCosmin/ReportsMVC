3af8f5e83e9b574a6546caed64813e665126f75d
Horatiu Vezentan <horatiu.vezentan@freshbyteinc.com>
2015-09-03 14:30:31 +0300

    partial view for email office

--- MVCReports/MVCReports/Scripts/report.behaviour.js
+++ MVCReports/MVCReports/Scripts/report.behaviour.js
@@ -1,51 +1,81 @@
-﻿    $(document).ready(function () {
-    	$("#generate-report-btn").click(function () {
-    	    var customers = getCustomers();
+﻿$(document).ready(function () {
+    var currentType = "";
 
-    	    $.post("/Home/GenerateReport",{ customers: JSON.stringify(customers) }, function (result) {
-    	        $("#report-center").html(result);
-    	    });
-    	});
+    $("#report-type-list li").each(function () {
+        var itemClass = $(this).attr('class');
 
-    	selectCheckBoxOnNameClick();
+        if (itemClass == "active") {
+            currentType = $(this).find('a').attr('name');
+        }
     });
 
-    function selectCheckBoxOnNameClick() {
-        $("#customers-list li").each(function () {   
-            $(this).click(function () {
-                var isChecked = $(this).find("#customer-check").is(":checked");
-                var name = $(this).find("#customer-name").text();
+    $("#generate-report-btn").click(function () {
+        sendCustomersFromList(currentType);
+    });
 
-                if (isChecked == true) {
-                    $(this).find("#customer-check").prop('checked', false);
-                } else {
-                    $(this).find("#customer-check").prop('checked', true);
-                }
-            });
+    selectCheckBoxOnNameClick();
+});
+
+function sendCustomersFromList(currentType) {
+    var customers = [];
+
+    switch (currentType) {
+        case "Email":
+            break;
+        case "Pie":
+            customers = getCustomers();
+            break;
+        case "Accuracy":
+            break;
+        case "Stacked":
+            customers = getCustomers();
+            break;
+        default:
+            break;
+    }
+   
+
+    $.post("/Home/GenerateReport", { customers: JSON.stringify(customers) }, function (result) {
+        $("#report-center").html(result);
+    });
+}
+
+function selectCheckBoxOnNameClick() {
+    $("#customers-list li").each(function () {
+        $(this).click(function () {
+            var isChecked = $(this).find("#customer-check").is(":checked");
+            var name = $(this).find("#customer-name").text();
+
+            if (isChecked == true) {
+                $(this).find("#customer-check").prop('checked', false);
+            } else {
+                $(this).find("#customer-check").prop('checked', true);
+            }
         });
+    });
+};
+
+function getCustomers() {
+    var customers = [];
+    var startDate = $("#datepickerStart").val();
+    var endDate = $("#datepickerEnd").val();
+
+    $("#customers-list li").each(function () {
+        var isChecked = $(this).find("#customer-check").is(":checked");
+        var name = $(this).find("#customer-name").text();
+        var customer = {
+            Name: name,
+            Checked: isChecked
+        };
+
+        customers.push(customer);
+    });
+
+    var customersObj = {
+        StartDate: startDate,
+        EndDate: endDate,
+        Customers: customers
     };
 
-    function getCustomers() {
-        var customers = [];
-        var startDate = $("#datepickerStart").val();
-        var endDate = $("#datepickerEnd").val();
-
-    	$("#customers-list li").each(function () {
-    	    var isChecked = $(this).find("#customer-check").is(":checked");
-    		var name = $(this).find("#customer-name").text();
-    		var customer = {
-    		    Name: name,
-                Checked : isChecked
-    		};
-
-    		customers.push(customer);
-    	});
-
-    	var customersObj = {
-    	    StartDate: startDate,
-            EndDate : endDate,
-            Customers : customers
-    	};
-
-    	return customersObj;
-    }
+    return customersObj;
+}
